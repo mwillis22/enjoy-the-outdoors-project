9598546885b845539a3acb05becc0b4618df2e1c
Mya Willis <mwillis@pgh.yearup.org>
2022-11-18 09:12:34 -0500

    parkspage.css

--- indexSearch.js
+++ indexSearch.js
@@ -2,3 +2,19 @@ import { nationalParksArray } from "./scripts/nationalParkData.js";
 import { locationsArray } from "./scripts/locationData.js";
 import { parkTypesArray } from "./scripts/parkTypeData.js";
 
+let locationsArrayLength = locationsArray.length;
+let parkTypesArrayLength = parkTypesArray.length;
+
+for (let i=0; i < locationsArrayLength; i ++) {
+  let newOption = new Option(locationsArray[i], locationsArray[i]);
+  let locationDropDown = document.getElementById('Location');
+  locationDropDown.add(newOption, undefined);
+}
+
+for (let i=0; i < parkTypesArrayLength; i ++) {
+  let newOption = new Option(parkTypesArray[i], parkTypesArray[i]);
+  let parkDropDown = document.getElementById('What');
+  parkDropDown.add(newOption, undefined);
+}
+
+
